c57d615ee26bf8e18c80ef24c8680d4dfbf90431
Roger Cordova <roger.cordova@totvs.com.br>
2025-12-21 14:22:53 -0300

    Ajustando dicionario e nomenclatua dos status

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             listar_local()
         elif opcao_escolhida == 3:
-            ativar_local()
+            troca_status_local()
         elif opcao_escolhida == 4:
             encerar_app()
         else: opcao_invalida()
@@ -37,9 +37,17 @@ def cadastrar_local():
     while (menu_principal == 1):
         os.system('cls')
         print('Cadastro de novos locais\n')
-        nome_do_local = input('Informe o nome do estabelecimento: ')
-        locais.append(nome_do_local)
+
+        nome_do_local = input(f'Informe o nome do estabelecimento: ')
+
+        categoria = input(f'Informe a categoria de {nome_do_local}: ')
+
+        dados_do_local = {'nome':nome_do_local,'categoria':categoria,'status':False}
+
+        locais.append(dados_do_local)
+
         print(f'\nO Estabelecimento {nome_do_local} foi cadastrado com sucesso\n')        
+
         menu_principal = int(input('Pressione 1 para cadastrar um novo locao ou pressione outra tecla para sair: '))
     else:    
         main()
@@ -48,13 +56,32 @@ def listar_local():
     os.system('cls')
     print('Listando os locais cadastrados\n')
     for local in locais:
-        print(f'{local}')
+        nome_do_local = local['nome']
+        categoria_do_local = local['categoria']
+        status_do_local = 'ativado' if local['status'] else 'desativado'
+        print(f'{nome_do_local}|{categoria_do_local}|{status_do_local}')
 
     input('\nPressione uma tecla para voltar ao menu principal: ')
     main()
 
-def ativar_local():
-    print('Ativar Restaurantes')
+def troca_status_local():
+    os.system('cls')
+    print('Troca Status Local')
+
+    nome_local = input('Informe o nome do local que deseja trocar o status: ')
+    local_encontrado = False
+
+    for local in locais:
+        if nome_local == local['nome']:
+           local_encontrado = True
+           local['status'] = not local['status']
+           mensagem = f'O local {nome_local} foi ativado com sucesso' if local['status'] else f'O local {nome_local} foi desativado com sucesso'
+           print(mensagem)
+    if not local_encontrado:
+        print(f'O local {nome_local} não foi encontrado')
+
+    input('\nPressione uma tecla para voltar ao menu principal: ')
+    main()            
 
 def encerar_app():
     os.system('cls')
