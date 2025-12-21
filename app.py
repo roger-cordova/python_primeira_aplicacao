66d74001b500ec469b6e5f1bcc35d98ea72f5a4c
Roger Cordova <roger.cordova@totvs.com.br>
2025-12-21 11:15:44 -0300

    inclusao do try except e listas

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+locais = []
 
 def exibir_nome_do_programa():
     print('''
@@ -17,17 +18,28 @@ def exibir_opcoes():
     print('4 - Sair')
 
 def escolher_opcoes():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    if opcao_escolhida == 1:
-        cadastrar_local()
-    elif opcao_escolhida == 2:
-        listar_local()
-    elif opcao_escolhida == 3:
-        ativar_local()
-    else: encerar_app()   
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            cadastrar_local()
+        elif opcao_escolhida == 2:
+            listar_local()
+        elif opcao_escolhida == 3:
+            ativar_local()
+        elif opcao_escolhida == 4:
+            encerar_app()
+        else: opcao_invalida()
+    except:
+        opcao_invalida()
 
 def cadastrar_local():
-    print('Cadastrar Restaurantes')
+    os.system('cls')
+    print('Cadastro de novos locais\n')
+    nome_do_local = input('Informe o nome do estabelecimento: ')
+    locais.append(nome_do_local)
+    print(f'O Estabelecimento {nome_do_local} foi cadastrado com sucesso\n')
+    input('Pressione uma tecla para voltar ao menu principal: ')
+    main()
 
 def listar_local():
     print('Listar Restairantes')
@@ -38,12 +50,17 @@ def ativar_local():
 def encerar_app():
     os.system('cls')
 
-
+def opcao_invalida():
+    print('Opção Inválida, por favor infore um valor de 1 a 4\n')
+    input('Pressione uma tecla para voltar ao menu principal: ')
+    main()
+    
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
-    escolher_opcoes
+    escolher_opcoes()
 
 if __name__ == '__main__':
     main()
